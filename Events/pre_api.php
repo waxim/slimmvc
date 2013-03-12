4e5741397d3152a132a3a6f782aabc2b6cba20a3
Alan Cole <alan@waxpolitical.com>
2013-03-12 17:55:58 +0000

    Revert "Added gitignore"
    This reverts commit 9f7047b7b46f96b71f0fa98121c889740d052a46.

---
+++ Events/pre_api.php
@@ -0,0 +1,9 @@
+<?php defined('BP') or exit('No direct script access allowed');
+
+	//Events::register('pre_system', array('Event', 'pre_system'));
+	//Events::register('pre_controller', array('Event', 'pre_controller'));
+	
+	//Events::register('after_includes', array(new Event, 'after_includes'));
+	//Events::register('after_includes', array(new Event, 'after_includes_two'));
+
+?>
